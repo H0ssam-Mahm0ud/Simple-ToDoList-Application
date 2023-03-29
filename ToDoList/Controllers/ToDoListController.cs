51ed85458b9cc5b696d41940b58c85e638358f3e
Hossam <103054530+H0ssam-Mahm0ud@users.noreply.github.com>
2023-03-29 22:14:54 +0200

    Add project files.

---
+++ ToDoList/Controllers/ToDoListController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoListController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // Get all tasks
+        public IActionResult Index()
+        {
+            var result = _context.ToDoLists.ToList();
+            return View(result);
+        }
+
+
+        // Get task by Id
+        public IActionResult TaskDetails(int? id)
+        {
+            if (id == null || _context.ToDoLists == null)
+            {
+                return NotFound();
+            }
+            var result = _context.ToDoLists.Find(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(result);
+        }
+
+
+        // Get 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TodoList todoList)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(todoList);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todoList);
+        }
+
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || _context.ToDoLists == null)
+            {
+                return NotFound();
+            }
+
+            var result = _context.ToDoLists.Find(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(result);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(TodoList todoList)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(todoList);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todoList);
+        }
+
+
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var result = _context.ToDoLists.Find(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(result);
+        }
+
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var result = _context.ToDoLists.Find(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(result);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ToDoList/Models/TodoList.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class TodoList
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="To Do")]
+        public string ToDo { get; set; }
+
+        [Display(Name = "Time")]
+        public DateTime DateTime { get; set; }
+    }
+}
